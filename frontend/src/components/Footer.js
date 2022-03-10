3c03e4075ca9349040738a81dcaf213945a42404
Ceana Daniel <careendaniel1996@gmail.com>
2022-03-10 16:53:18 -0600

    Phase 1 of frontend complete

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 
+
 const Footer = () => {
   return (
     <footer>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar,Container, Nav } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 
 const Header = () => {
@@ -7,17 +8,21 @@ const Header = () => {
     <header>
          <Navbar bg="black" variant="black" expand="lg" collapseOnSelect>
         <Container>
-     
+          <LinkContainer to= '/'>
            <Navbar.Brand>MyShop</Navbar.Brand>
+           </LinkContainer>
         
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
              
+            <LinkContainer to= '/cart'>
                 <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-             
-             
+                </LinkContainer>
+
+              <LinkContainer to= '/login'>
                 <Nav.Link ><i className='fas fa-user'></i> Sign In</Nav.Link>
+                </LinkContainer>
               
             </Nav>
           </Navbar.Collapse>
